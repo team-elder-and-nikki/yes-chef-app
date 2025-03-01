28e425c34d432fd95a6aba1d4fd9570a1817c508
Richard <66279068+choir241@users.noreply.github.com>
2025-02-28 20:01:48 -0500

    Update app.js
    Co-authored-by: Thomas Harris <68450309+LodenH16@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const app = express();
-const PORT = 8000;
 const bodyParser = require("body-parser");
+
+const PORT = 8000;
+const app = express();
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
