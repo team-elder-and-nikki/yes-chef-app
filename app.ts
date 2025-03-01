c95eab8ec3d2afa4610cff81a8fc220a8d928847
Jamal Hall <JamalHallWebDev@gmail.com>
2025-03-01 15:54:02 -0500

    added DB connection

--- app.ts
+++ app.ts
@@ -1,23 +1,30 @@
 import express from 'express';
-import { SERVER } from './config/config.js';
+import { SERVER, DB_Connect } from './config/config.js';
 
 const PORT = SERVER.SERVER_PORT;
+async function startServer() {
 
-try {
-    
-	console.info('Starting Express Application');
-	const app = express();
+    try {
+		console.log('Starting Express Application');
+		const app = express();
 
-	app.use(express.urlencoded({ extended: true }));
-	app.use(express.json());
+		app.use(express.urlencoded({ extended: true }));
+		app.use(express.json());
 
-	app.listen(PORT, () => {
-		console.log(`The Server is running use ^c to chill server`);
-		console.log(`Server started on ${SERVER.SERVER_HOSTNAME}:${PORT}`);
-		
-    });
-    
-} catch (err) {
-	console.error('Failed to start server:', err);
-	process.exit(1);
+		await app.listen(PORT, () => {
+			console.log(`The Server is running use ^c to chill server`);
+			console.log(`Server started on ${SERVER.SERVER_HOSTNAME}:${PORT}`);		
+		});
+	
+		// Database connection	
+		console.log('Connect to the database');
+		await DB_Connect();
+
+		return app;
+	} catch (err) {
+		console.error('Failed to start server:', err);
+		process.exit(1);
+	}
 }
+
+startServer();
--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,27 @@
+import dotenv from 'dotenv';
+const env = './config/.env';
+dotenv.config({ path: env });
+import mongoose from 'mongoose';
+
 export const SERVER = {
 	SERVER_HOSTNAME: process.env.SERVER_HOSTNAME || 'localhost',
 	SERVER_PORT: process.env.PORT || 8000,	
+};
+
+export const DB_Connect = async () => {   
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI!, {
+            ssl: true, 
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 45000,
+        });
+        console.info(`MongoDB Connected: ${conn.connection.name}`);
+        console.info(`MongoDB Connected: ${JSON.stringify(conn.connection.models)}`);
+        console.info(`MongoDB Connected: ${conn.connection.host}`);
+        return 'Connected';
+    } catch (err) {
+        console.error('Failed to connect to MongoDB:', err);
+        process.exit(1); 
+    }
 };
