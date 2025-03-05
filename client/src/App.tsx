86c6d98e6087905237c82bc40b8d5d3343261107
Richard <richardchoi54@gmail.com>
2025-03-05 17:16:27 -0500

    download react-router package. setup app routes for pages.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
+import { Route, Routes, BrowserRouter } from "react-router";
 import "./App.css";
+import Menu from "./pages/Menu";
+import Inventory from "./pages/Inventory";
+import Reports from "./pages/Reports";
+import Orders from "./pages/Orders";
+import Kitchen from "./pages/Kitchen";
+// Example: import { Button } from "@/components/ui/button";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Menu />}></Route>
+        <Route path="/inventory" element={<Inventory />}></Route>
+        <Route path="/kitchen" element={<Kitchen />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+        <Route path="/reports" element={<Reports />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
