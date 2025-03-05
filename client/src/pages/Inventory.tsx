44ccf97d005e98fe0aaa336d2db19ad958a70130
Richard <richardchoi54@gmail.com>
2025-03-05 12:57:16 -0500

    download react-router package. setup app routes for pages.

---
+++ client/src/pages/Inventory.tsx
@@ -0,0 +1,7 @@
+export default function Inventory(){
+    return(
+        <main>
+            <h1>Inventory</h1>
+        </main>
+    )
+}
---
+++ client/src/pages/Kitchen.tsx
@@ -0,0 +1,7 @@
+export default function Kitchen(){
+    return(
+        <main>
+            <h1>Kitchen</h1>
+        </main>
+    )
+}
