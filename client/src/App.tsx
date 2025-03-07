a13d9b971f7bda9bd9b11053f509c33722c1b33c
Anton <shelkovnikov9108@gmail.com>
2025-03-06 22:31:37 -0500

    wrap App with cartProvider component for global context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { CartProvider } from "./context/CartContext";
+import { CartTable, } from "./components/CartTable";
+import { MenuItem } from "./components/MenuItemTest";
+
+
 
 function App() {
-  return <></>;
+  return (
+    <CartProvider>
+      <div className="App">
+        <h1>POS System</h1>
+        <MenuItem />
+        <CartTable />
+      </div>
+    </CartProvider>
+  );
 }
+
 
 export default App;
