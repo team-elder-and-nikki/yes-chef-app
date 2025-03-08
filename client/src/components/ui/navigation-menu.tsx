5bd9e9e781affa73425135b688309af3d50e1d5b
Thomas Harris <loden16harris@gmail.com>
2025-03-08 13:03:48 -0600

    center nav elements horizontally

--- client/src/components/ui/navigation-menu.tsx
+++ client/src/components/ui/navigation-menu.tsx
@@ -18,7 +18,8 @@ function NavigationMenu({
       data-slot="navigation-menu"
       data-viewport={viewport}
       className={cn(
-        "group/navigation-menu relative flex max-w-max flex-1 items-center justify-center",
+        `group/navigation-menu relative flex flex-1 items-center justify-center 
+        w-screen`,
         className
       )}
       {...props}
