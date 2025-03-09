f2f9e2dd91f5c4378b441873715645e79618b367
Megan McNeill <mcneim2@gmail.com>
2025-03-09 10:54:00 -0400

    styling

--- client/src/components/MobileInventoryCard.tsx
+++ client/src/components/MobileInventoryCard.tsx
@@ -37,38 +37,46 @@ export function MobileInventoryCard() {
   }, []);
 
   return (
-    <Accordion type="single" collapsible className="w-full">
-      {ingredients.map((ingredient) => (
-        <AccordionItem key={ingredient._id} value={ingredient._id}>
-          <AccordionTrigger className="grid grid-cols-5 items-center text-center w-full">
-            <div className="text-center">{ingredient.name}</div>
-            <div className="flex justify-center">
-              <Badge className="flex items-center justify-center">
-                {ingredient.quantity}
-              </Badge>
-            </div>
-            <div>last order date</div>
-            <div>${ingredient.unitCost.toFixed(2)}</div>
-          </AccordionTrigger>
-          <AccordionContent className="flex flex-row justify-around items-center">
-            <div className="flex-col justify-items-center">
-              <div>Unit Cost</div>
-              <div>${ingredient.unitCost.toFixed(2)}</div>
-            </div>
-            {/* reorderqty should be an input field or a +/- */}
-            <div className="flex-col justify-items-center">
+    <>
+      <div className="grid grid-cols-4 items-center text-center w-full">
+        <div>Item</div>
+        <div>In Stock</div>
+        <div>Last Order</div>
+      </div>
+      <Accordion type="single" collapsible className="w-full">
+        {ingredients.map((ingredient) => (
+          <AccordionItem key={ingredient._id} value={ingredient._id}>
+            <AccordionTrigger className="grid grid-cols-4 items-center text-center w-full">
+              <div className="text-center">{ingredient.name}</div>
+              <div className="flex justify-center">
+                <Badge className="flex items-center justify-center">
+                  {ingredient.quantity}
+                </Badge>
+              </div>
+              <div>last order date</div>
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-row justify-around items-center">
+              <div className="flex-col justify-items-center">
+                <div>Unit Cost</div>
+                <div>${ingredient.unitCost.toFixed(2)}</div>
+              </div>
+              {/* reorderqty should be an input field or a +/- */}
+              <div className="flex-col justify-items-center">
                 <div>reorder qty</div>
                 <div>input box</div>
-            </div>
-            <div className="flex-col justify-items-center">
-              <div>total cost</div>
-              <div>${(ingredient.unitCost * ingredient.quantity).toFixed(2)}</div>
-            </div>
-            {/* hitting shopping cart icon should send the order to the distributor api */}
-            <ShoppingCartIcon></ShoppingCartIcon>
-          </AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
+              </div>
+              <div className="flex-col justify-items-center">
+                <div>total cost</div>
+                <div>
+                  ${(ingredient.unitCost * ingredient.quantity).toFixed(2)}
+                </div>
+              </div>
+              {/* hitting shopping cart icon should send the order to the distributor api */}
+              <ShoppingCartIcon></ShoppingCartIcon>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
   );
 }
