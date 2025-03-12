7231b8ec219fffb69d831e73c7fb05f1c279a8ef
Thomas Harris <loden16harris@gmail.com>
2025-03-12 18:08:17 -0500

    change header and footer color depending on ticket status

--- client/src/components/KitchenCard.tsx
+++ client/src/components/KitchenCard.tsx
@@ -2,13 +2,36 @@ import { Card, CardHeader, CardFooter, CardTitle, CardContent } from "@/componen
 import { Button } from "./ui/button"
 import { Table, TableHeader, TableHead, TableRow, TableBody, TableCell, TableCaption } from "@/components/ui/table"
 import { ITicket } from "../models/Ticket"
+import { useEffect, useState } from "react"
 
 
 export default function KitchenCard({ ticket }: { ticket: ITicket }) {
+    const headerFooterColorOptions = {
+        "Unstarted": "bg-red-300",
+        "In Progress": "bg-yellow-300",
+        "Completed": "bg-green-300"
+    }
+    const [headerFooterColor, setHeaderFooterColor] = useState(headerFooterColorOptions[ticket.status])
+    useEffect(() => {
+        switch (ticket.status) {
+            case "Unstarted":
+                setHeaderFooterColor(headerFooterColorOptions["Unstarted"]);
+                break;
+            case "In Progress":
+                setHeaderFooterColor(headerFooterColorOptions["In Progress"])
+                break;
+            case "Completed":
+                setHeaderFooterColor(headerFooterColorOptions["Completed"]);
+                break;
+            default:
+                setHeaderFooterColor(headerFooterColorOptions["Unstarted"]);
+                break;
+        }
+    }, [ticket.status])
 
     return (
         <Card className="size-max h-fit max-h-fit py-0">
-            <CardHeader className="rounded-t-xl py-3 flex flex-row justify-between bg-amber-300">
+            <CardHeader className={"rounded-t-xl py-3 flex flex-row justify-between " + headerFooterColor}>
                 <CardTitle className="w-max">{`#${ticket.ticket_number}`}</CardTitle>
                 <CardTitle className="w-max">{`${ticket.ordered_at.getHours()}:${ticket.ordered_at.getMinutes()}`}</CardTitle>
             </CardHeader>
@@ -29,7 +52,7 @@ export default function KitchenCard({ ticket }: { ticket: ITicket }) {
                 </Table>
                 <Button> Change Status</Button>
             </CardContent>
-            <CardFooter className="rounded-b-xl py-3 bg-amber-300 justify-center">
+            <CardFooter className={"rounded-b-xl py-3 justify-center " + headerFooterColor}>
                 <CardTitle className="text-center">{ticket.status}</CardTitle>
             </CardFooter>
         </Card>
