db3335426579cf6ab6b52dc0b8743eea464a8193
Megan McNeill <mcneim2@gmail.com>
2025-03-11 21:39:52 -0400

    styled inventory mobile page, added pagination

--- client/src/pages/Inventory.tsx
+++ client/src/pages/Inventory.tsx
@@ -9,9 +9,9 @@ export default function Inventory(){
         <>
          <NavBar />
             <div className="md:ml-21"/*bump everything to the right when NavBar is fixed to the left*/>
-                <h1>Inventory</h1>
+                <h1 className="text-center text-2xl my-4">Inventory</h1>
                 {/* Mobile View: Show only on screens smaller than md (≤ 768px) */}
-                <div className="block md:hidden">
+                <div className="block md:hidden mx-4">
                     <MobileInventoryCard />
                 </div>
 
