0707fb003d35d07c554a31dbe19c2e386e990323
Thomas Harris <loden16harris@gmail.com>
2025-03-11 20:22:48 -0500

    get types and dummy information working

--- client/src/pages/Kitchen.tsx
+++ client/src/pages/Kitchen.tsx
@@ -1,85 +1,116 @@
 import NavBar from "@/components/NavBar"
 import KitchenCard from "@/components/KitchenCard";
-import { ITicket } from "../../../server/models/Ticket";
+import { ITicket } from "../models/Ticket";
 
 export default function Kitchen() {
 
-    const dummyTicketData: ITicket = [
-        {
-            _id: "uniqueTicketId",
-            ticket_number: 123,
-            ordered_at: new Date(),
-            menu_items: [
-                {
-                    _id: "uniqueMenuItemId",
-                    name: "Burger",
-                    ingredients: [
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Bun",
-                            unitCost: 1,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Patty",
-                            unitCost: 2,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Lettuce",
-                            unitCost: 1,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Tomato",
-                            unitCost: 1,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Cheese",
-                            unitCost: 1,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Onion",
-                            unitCost: 1,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                        {
-                            _id: "uniqueIngredientId",
-                            name: "Pickles",
-                            unitCost: 1,
-                            quantity: 1,
-                            thresholdLevel: 10,
-                        },
-                    ],
-                    quantity: 1,
-                    price: 10,
-                    prepTime: 15,
-                    Image: "burger.jpg",
-                    category: "Main",
-                }
-            ],
-            status: "Unstarted",
-        }
-    ];
+    const dummyTicketData: ITicket =
+    {
+        _id: "uniqueTicketId",
+        ticket_number: 123,
+        ordered_at: new Date(),
+        menu_items: [
+            {
+                _id: "uniqueMenuItemId",
+                name: "Burger",
+                ingredients: [
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Bun",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Patty",
+                        unitCost: 2,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Lettuce",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Tomato",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Cheese",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Onion",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Pickles",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                ],
+                quantity: 1,
+                price: 10,
+                prepTime: 15,
+                Image: "burger.jpg",
+                category: "Main",
+            },
+            {
+                _id: "uniqueMenuItemId",
+                name: "Fries",
+                ingredients: [
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Potato",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                    {
+                        _id: "uniqueIngredientId",
+                        name: "Salt",
+                        unitCost: 1,
+                        quantity: 1,
+                        thresholdLevel: 10,
+                    },
+                ],
+                quantity: 1,
+                price: 5,
+                prepTime: 10,
+                Image: "fries.jpg",
+                category: "Side",
+            }
+        ],
+        status: "Unstarted",
+    }
+
+    const multipleDummyTickets = [
+        dummyTicketData,
+        dummyTicketData,
+        dummyTicketData,
+        dummyTicketData,
+    ]
 
     return (
         <>
             <NavBar />
             <div className="md:ml-21"/*bump everything to the right when NavBar is fixed to the left*/>
-                {dummyTicketData.map((ticket) => (
+                {multipleDummyTickets.map((ticket) => (
                     <KitchenCard key={ticket._id} ticket={ticket} />
                 ))}
             </div>
