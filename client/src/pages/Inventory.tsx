71e467e25838f3c12e9da0531a47d99840718b95
Megan McNeill <mcneim2@gmail.com>
2025-03-11 21:14:51 -0400

    responsive rendering of mobile or tablet-desktop inventory views

--- client/src/pages/Inventory.tsx
+++ client/src/pages/Inventory.tsx
@@ -10,10 +10,16 @@ export default function Inventory(){
          <NavBar />
             <div className="md:ml-21"/*bump everything to the right when NavBar is fixed to the left*/>
                 <h1>Inventory</h1>
-                <MobileInventoryCard/>
-                <InventoryTable />
+                {/* Mobile View: Show only on screens smaller than md (≤ 768px) */}
+                <div className="block md:hidden">
+                    <MobileInventoryCard />
+                </div>
+
+                {/* Tablet & Larger: Show only on screens md (≥ 768px) and up */}
+                <div className="hidden md:block">
+                    <InventoryTable />
+                </div>
            </div>
-            
         </>
     )
 }
