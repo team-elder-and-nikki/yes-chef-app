f2f73ba3a31cc2d84e5c648f9cffd84e4fac9260
Anton <shelkovnikov9108@gmail.com>
2025-03-20 11:50:15 -0400

    add react query and query provide to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,23 +6,27 @@ import Reports from "./pages/Reports";
 import Orders from "./pages/Orders";
 import Kitchen from "./pages/Kitchen";
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CartProvider } from "./context/CartContext";
 
 // Example: import { Button } from "@/components/ui/button";
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Menu />}></Route>
-          <Route path="/inventory" element={<Inventory />}></Route>
-          <Route path="/kitchen" element={<Kitchen />}></Route>
-          <Route path="/orders" element={<Orders />}></Route>
-          <Route path="/reports" element={<Reports />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <QueryClientProvider client={queryClient} >
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Menu />}></Route>
+            <Route path="/inventory" element={<Inventory />}></Route>
+            <Route path="/kitchen" element={<Kitchen />}></Route>
+            <Route path="/orders" element={<Orders />}></Route>
+            <Route path="/reports" element={<Reports />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </QueryClientProvider>
   );
 }
 
