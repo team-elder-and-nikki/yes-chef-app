00e95dd8270b0a9680a757718f0ffc767a06f045
Anton <shelkovnikov9108@gmail.com>
2025-03-21 16:30:48 -0400

    remove Header component

--- client/src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <div className="text-left py-4 bg-white shadow-md">
-            <div className=" ml-2 text-xl font-bold text-gray-800"><h1>YES CHEF</h1></div>
-        </div>
-    )
-}
